416ec6210d1443e3182a32320ad9b3918de40c07
laumc98 <lanaca98@gmail.com>
2022-03-07 16:40:26 -0500

    job stats prime querys

---
+++ opps_commited_review_date_prime.sql
@@ -0,0 +1,7 @@
+SELECT str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_changes_history`.`opportunity_id`) AS `opps_commited_review_date_prime`
+FROM `opportunity_changes_history`
+LEFT JOIN `opportunities` `opportunities__via__opportunit` ON `opportunity_changes_history`.`opportunity_id` = `opportunities__via__opportunit`.`id`
+WHERE (`opportunity_changes_history`.`type` = 'commit'
+   AND `opportunities__via__opportunit`.`reviewed` >= date(date_add(now(6), INTERVAL -90 day)) AND `opportunities__via__opportunit`.`reviewed` < date(now(6)) AND `opportunities__via__opportunit`.`fulfillment` = 'prime')
+GROUP BY str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W')
+ORDER BY str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W') ASC
