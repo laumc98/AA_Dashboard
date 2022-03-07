e05a4b2112fb9c5dfc944f1b03459d38505bad27
laumc98 <lanaca98@gmail.com>
2022-03-07 11:04:46 -0500

    job stats remote querys

---
+++ opps_commited_weekly_remote.sql
@@ -0,0 +1,7 @@
+SELECT str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_changes_history`.`opportunity_id`) AS `opps_commited_weekly_remote`
+FROM `opportunity_changes_history`
+LEFT JOIN `opportunities` `opportunities__via__opportunit` ON `opportunity_changes_history`.`opportunity_id` = `opportunities__via__opportunit`.`id`
+WHERE (`opportunity_changes_history`.`type` = 'commit'
+   AND `opportunities__via__opportunit`.`reviewed` >= date(date_add(now(6), INTERVAL -90 day)) AND `opportunities__via__opportunit`.`reviewed` < date(now(6)) AND `opportunities__via__opportunit`.`remote` = TRUE)
+GROUP BY str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W')
+ORDER BY str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W') ASC
