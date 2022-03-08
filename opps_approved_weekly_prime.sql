0a4d458ab30c3e960645658486ebf866fb35a6de
laumc98 <lanaca98@gmail.com>
2022-03-07 19:34:00 -0500

    Revert "change time interval"
    This reverts commit 93e1229aee41cb9d5be3d3f264c6c51952ef4430.

--- opps_approved_weekly_prime.sql
+++ opps_approved_weekly_prime.sql
@@ -2,7 +2,7 @@ SELECT str_to_date(concat(yearweek(`opportunities`.`reviewed`), ' Sunday'), '%X%
 FROM `opportunities`
 LEFT JOIN `opportunity_organizations` `Opportunity Organizations` ON `opportunities`.`id` = `Opportunity Organizations`.`opportunity_id`
 WHERE (`opportunities`.`reviewed` IS NOT NULL
-   AND `opportunities`.`reviewed` >= date(date_add(now(6), INTERVAL -90 day)) AND `opportunities`.`reviewed` < date(date_add(now(6), INTERVAL 1 day)) AND `opportunities`.`review` = 'approved' AND (`Opportunity Organizations`.`organization_id` <> 665801
+   AND `opportunities`.`reviewed` >= date(date_add(now(6), INTERVAL -60 day)) AND `opportunities`.`reviewed` < date(date_add(now(6), INTERVAL 1 day)) AND `opportunities`.`review` = 'approved' AND (`Opportunity Organizations`.`organization_id` <> 665801
     OR `Opportunity Organizations`.`organization_id` IS NULL) AND `opportunities`.`fulfillment` = 'prime')
 GROUP BY str_to_date(concat(yearweek(`opportunities`.`reviewed`), ' Sunday'), '%X%V %W')
 ORDER BY str_to_date(concat(yearweek(`opportunities`.`reviewed`), ' Sunday'), '%X%V %W') ASC
