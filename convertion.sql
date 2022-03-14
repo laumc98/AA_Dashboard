ace22984d780e71fe30f446d882b7312ee574906
laumc98 <lanaca98@gmail.com>
2022-03-14 14:06:30 -0500

    Update convertion.sql

--- convertion.sql
+++ convertion.sql
@@ -1,7 +1,6 @@
 SELECT 
 `Users User - Referred`.`id` AS  `SubjectID`,
 `referrers_referrer`.`intent` AS `intent`, 
-`Users User - Referred`.`email` AS `email`
 FROM `referrers_referrer`  
 LEFT JOIN `users_user` `Users User - Referred` ON `referrers_referrer`.`referred_id` = `Users User - Referred`.`id`
 LIMIT 1048575
