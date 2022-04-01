26530fa9aaafcfe9f3fe1fce2f53ad144341e140
laumc98 <lanaca98@gmail.com>
2022-04-01 12:59:16 -0500

    changes commited where clause

--- opps_commited_weekly.sql
+++ opps_commited_weekly.sql
@@ -1,7 +1,7 @@
 SELECT str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_changes_history`.`opportunity_id`) AS `opps_commited_weekly`
 FROM `opportunity_changes_history`
 LEFT JOIN `opportunities` `opportunities__via__opportunit` ON `opportunity_changes_history`.`opportunity_id` = `opportunities__via__opportunit`.`id`
-WHERE (`opportunity_changes_history`.`type` = 'commit'
+WHERE (`opportunity_changes_history`.`type` = 'commit' AND  `opportunity_changes_history`.`value` = 0   
    AND `opportunities__via__opportunit`.`reviewed` >= date(date_add(now(6), INTERVAL -262 day)) AND `opportunities__via__opportunit`.`reviewed` < date(now(6)))
 GROUP BY str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W')
 ORDER BY str_to_date(concat(yearweek(`opportunity_changes_history`.`created`), ' Sunday'), '%X%V %W') ASC
