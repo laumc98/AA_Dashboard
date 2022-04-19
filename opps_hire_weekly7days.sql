e402166aa7de02a57e6ce1013bc71992893089ad
laumc98 <lanaca98@gmail.com>
2022-04-19 13:17:48 -0500

    changes

--- opps_hire_weekly7days.sql
+++ opps_hire_weekly7days.sql
@@ -3,6 +3,6 @@ FROM `opportunity_stats_hires`
 LEFT JOIN `opportunities` `Opportunities` ON `opportunity_stats_hires`.`opportunity_id` = `Opportunities`.`id`
 WHERE (`opportunity_stats_hires`.`hiring_date` > "2021-7-18"
    AND `opportunity_stats_hires`.`hiring_date` < date(now(6))
-   AND datediff(date(`opportunity_stats_hires`.`hiring_date`),date(`Opportunities`.`reviewed`)) = 7)
+   AND datediff(date(`opportunity_stats_hires`.`hiring_date`),date(`Opportunities`.`reviewed`)) <= 7)
 GROUP BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W')
 ORDER BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W') ASC
