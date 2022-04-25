c5f57b6ddaef268e5c0106eba897ccd96e93f8db
laumc98 <lanaca98@gmail.com>
2022-04-25 13:40:33 -0500

    change commit to outbound label

--- opps_commited_review_date.sql
+++ opps_commited_review_date.sql
@@ -1,7 +1,7 @@
 SELECT str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_changes_history`.`opportunity_id`) AS `opps_commited_review_date`
 FROM `opportunity_changes_history`
 LEFT JOIN `opportunities` `opportunities__via__opportunit` ON `opportunity_changes_history`.`opportunity_id` = `opportunities__via__opportunit`.`id`
-WHERE (`opportunity_changes_history`.`type` = 'commit' AND  `opportunity_changes_history`.`value` = 0   
+WHERE (`opportunity_changes_history`.`type` = 'outbound' AND  `opportunity_changes_history`.`value` = 0   
    AND `opportunities__via__opportunit`.`reviewed` > "2021-7-18" AND `opportunities__via__opportunit`.`reviewed` < date(now(6)))
 GROUP BY str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W')
 ORDER BY str_to_date(concat(yearweek(`opportunities__via__opportunit`.`reviewed`), ' Sunday'), '%X%V %W') ASC
