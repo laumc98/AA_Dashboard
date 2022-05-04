b76042ac79f793f50e8902a9e5fb76cdf9d841fc
laumc98 <lanaca98@gmail.com>
2022-05-04 13:49:23 -0500

    general unsubmitted prime

---
+++ general_unsubmitted_prime.sql
@@ -0,0 +1,18 @@
+select
+    str_to_date(concat(yearweek(`opportunity_candidates`.`created`),' Sunday'),'%X%V %W') as date,
+    IF(ISNULL(interested), 'started', 'finished') as finished,
+    count(distinct opportunity_candidates.id) as applications
+from
+    opportunity_candidates
+    inner join opportunities as o on opportunity_candidates.opportunity_id = o.id
+    left join tracking_code_candidates as tcc
+    left join tracking_codes as tc on tcc.tracking_code_id = tc.id on tcc.candidate_id = opportunity_candidates.id
+    left join opportunity_members on o.id = opportunity_members.opportunity_id
+    and poster = 1
+    left join people on opportunity_members.person_id = people.id
+    left join person_flags on people.id = person_flags.person_id
+where
+    date(opportunity_candidates.created) > "2021-7-18"
+    AND o.objective not like '***%'
+    and (o.fulfillment = 'prime')
+group by 1,2
