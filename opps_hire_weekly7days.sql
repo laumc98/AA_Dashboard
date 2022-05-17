d9cf6c9ad4a242f1b886ba769c91af11cfa83b07
laumc98 <lanaca98@gmail.com>
2022-05-17 10:18:54 -0500

    add hires within 15 days approval

--- opps_hire_weekly7days.sql
+++ opps_hire_weekly7days.sql
@@ -1,8 +1,19 @@
-SELECT str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_stats_hires`.`opportunity_id`) AS `opps_hire_weekly7days`
-FROM `opportunity_stats_hires`
-LEFT JOIN `opportunities` `Opportunities` ON `opportunity_stats_hires`.`opportunity_id` = `Opportunities`.`id`
-WHERE (`opportunity_stats_hires`.`hiring_date` > "2021-7-18"
-   AND `opportunity_stats_hires`.`hiring_date` < date(now(6))
-   AND datediff(date(`opportunity_stats_hires`.`hiring_date`),date(`Opportunities`.`reviewed`)) <= 7)
-GROUP BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W')
-ORDER BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W') ASC
+SELECT
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W') AS `date`,
+   count(distinct `opportunity_stats_hires`.`opportunity_id`) AS `opps_hire_weekly7days`
+FROM
+   `opportunity_stats_hires`
+   LEFT JOIN `opportunities` `Opportunities` ON `opportunity_stats_hires`.`opportunity_id` = `Opportunities`.`id`
+WHERE
+   (
+      `opportunity_stats_hires`.`hiring_date` > "2021-7-18"
+      AND `opportunity_stats_hires`.`hiring_date` < date(now(6))
+      AND datediff(
+         date(`opportunity_stats_hires`.`hiring_date`),
+         date(`Opportunities`.`reviewed`)
+      ) <= 7
+   )
+GROUP BY
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W')
+ORDER BY
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W') ASC
--- opps_hire_weekly_prime7days.sql
+++ opps_hire_weekly_prime7days.sql
@@ -1,9 +1,20 @@
-SELECT str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W') AS `date`, count(distinct `opportunity_stats_hires`.`opportunity_id`) AS `opps_hire_weekly_prime7days`
-FROM `opportunity_stats_hires`
-LEFT JOIN `opportunities` `Opportunities` ON `opportunity_stats_hires`.`opportunity_id` = `Opportunities`.`id`
-WHERE (`opportunity_stats_hires`.`hiring_date` > "2021-7-18"
-   AND `opportunity_stats_hires`.`hiring_date` < date(now(6))
-   AND datediff(date(`opportunity_stats_hires`.`hiring_date`),date(`Opportunities`.`reviewed`)) <= 7
-   AND `Opportunities`.`fulfillment` = 'prime')
-GROUP BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W')
-ORDER BY str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`), ' Sunday'), '%X%V %W') ASC
+SELECT
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W') AS `date`,
+   count(distinct `opportunity_stats_hires`.`opportunity_id`) AS `opps_hire_weekly_prime7days`
+FROM
+   `opportunity_stats_hires`
+   LEFT JOIN `opportunities` `Opportunities` ON `opportunity_stats_hires`.`opportunity_id` = `Opportunities`.`id`
+WHERE
+   (
+      `opportunity_stats_hires`.`hiring_date` > "2021-7-18"
+      AND `opportunity_stats_hires`.`hiring_date` < date(now(6))
+      AND datediff(
+         date(`opportunity_stats_hires`.`hiring_date`),
+         date(`Opportunities`.`reviewed`)
+      ) <= 7
+      AND `Opportunities`.`fulfillment` = 'prime'
+   )
+GROUP BY
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W')
+ORDER BY
+   str_to_date(concat(yearweek(`opportunity_stats_hires`.`hiring_date`),'Sunday'),'%X%V %W') ASC
