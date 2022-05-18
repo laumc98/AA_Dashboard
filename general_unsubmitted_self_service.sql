e24a2c4fe4aefee27b4d4afade4e176d52e8166c
laumc98 <lanaca98@gmail.com>
2022-05-18 11:15:09 -0500

    Update general_unsubmitted_self_service.sql

--- general_unsubmitted_self_service.sql
+++ general_unsubmitted_self_service.sql
@@ -14,6 +14,6 @@ from
 where
     date(opportunity_candidates.created) > "2021-7-18"
     AND o.objective not like '***%'
-    AND (o.fulfillment = 'prime')
+    AND (o.fulfillment = 'self_service')
     AND opportunity_candidates.application_step IS NOT NULL
 group by 1,2
