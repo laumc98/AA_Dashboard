969e2c30ce480cf87120108dbab48d7d3dc816f5
laumc98 <lanaca98@gmail.com>
2022-06-02 08:42:14 -0500

    Formatting

--- convertion.sql
+++ convertion.sql
@@ -1,6 +1,6 @@
-SELECT 
-`Users User - Referred`.`id` AS  `SubjectID`,
-`referrers_referrer`.`intent` AS `intent`
-FROM `referrers_referrer`  
-LEFT JOIN `users_user` `Users User - Referred` ON `referrers_referrer`.`referred_id` = `Users User - Referred`.`id`
-LIMIT 1048575
+SELECT
+    `Users User - Referred`.`id` AS `SubjectID`,
+    `referrers_referrer`.`intent` AS `intent`
+FROM
+    `referrers_referrer`
+    LEFT JOIN `users_user` `Users User - Referred` ON `referrers_referrer`.`referred_id` = `Users User - Referred`.`id`
