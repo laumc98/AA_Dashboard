c09b09fa126ec9223ffc5b2ad95ff2ff2a576980
laumc98 <lanaca98@gmail.com>
2022-06-02 18:00:39 -0500

    general unsubmitted prime filtered

--- weekly_app_channel_remote.sql
+++ weekly_app_channel_remote.sql
@@ -1,7 +1,7 @@
 /* AA : AA Main dashboard : weekly remote app per channel : prod */ 
 SELECT
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W') AS `date`,
-    `source`.`opportunity_id` AS `ID`,
+    /*`source`.`opportunity_id` AS `ID`*/
     `source`.`Tracking Codes__utm_medium` AS `Tracking Codes__utm_medium`,
     count(distinct `source`.`id`) AS `weekly_app_channel_remote`
 FROM
@@ -40,8 +40,8 @@ WHERE
     )
 GROUP BY
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W'),
-    `source`.`Tracking Codes__utm_medium`,
-    `source`.`opportunity_id`
+    `source`.`Tracking Codes__utm_medium`
+    /*`source`.`opportunity_id` AS `ID`*/
 ORDER BY
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W') ASC,
     `source`.`Tracking Codes__utm_medium` ASC
