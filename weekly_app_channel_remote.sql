84e8c9a765bb4dbb169baffb3a1a0589ce2a3ba9
laumc98 <lanaca98@gmail.com>
2022-06-03 09:56:12 -0500

    Update weekly_app_channel_remote.sql

--- weekly_app_channel_remote.sql
+++ weekly_app_channel_remote.sql
@@ -1,7 +1,7 @@
 /* AA : AA Main dashboard : weekly remote app per channel : prod */ 
 SELECT
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W') AS `date`,
-    /*`source`.`opportunity_id` AS `ID`*/
+    `source`.`opportunity_id` AS `ID`,
     `source`.`Tracking Codes__utm_medium` AS `Tracking Codes__utm_medium`,
     count(distinct `source`.`id`) AS `weekly_app_channel_remote`
 FROM
@@ -35,13 +35,14 @@ FROM
     ) `source`
 WHERE
     (
-        `source`.`interested` > "2021-7-18"
+        `source`.`Opportunities__remote` = 1
+        AND `source`.`interested` > "2021-7-18"
         AND `source`.`interested` < date(date_add(now(6), INTERVAL 1 day))
     )
 GROUP BY
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W'),
-    `source`.`Tracking Codes__utm_medium`
-    /*`source`.`opportunity_id` AS `ID`*/
+    `source`.`Tracking Codes__utm_medium`,
+    `source`.`opportunity_id`
 ORDER BY
     str_to_date(concat(yearweek(`source`.`interested`), ' Sunday'),'%X%V %W') ASC,
     `source`.`Tracking Codes__utm_medium` ASC
