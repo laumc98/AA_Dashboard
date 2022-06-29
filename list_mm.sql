a8b6d8fff40f8c822d91ec34f895bb9d55b23560
laumc98 <lanaca98@gmail.com>
2022-06-29 10:57:06 -0500

    Update in list of 3mm

--- list_mm.sql
+++ list_mm.sql
@@ -10,3 +10,19 @@ WHERE
     oc.name = 'mutual matches'
     AND occh.created >= '2021-06-20'
     AND o.objective NOT LIKE '**%'
+    AND o.id IN (
+        SELECT
+            DISTINCT o.id AS opportunity_id
+        FROM
+            opportunities o
+            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
+            AND omp.poster = TRUE
+            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
+            AND pf.opportunity_crawler = FALSE
+        WHERE
+            o.reviewed >= '2021/01/01'
+            AND o.objective NOT LIKE '**%'
+            AND o.review = 'approved'
+    )
+GROUP BY
+    occh.candidate_id
