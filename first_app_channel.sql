cbd5b2eed62e11899a18236f2ef0816acb5dc08c
laumc98 <lanaca98@gmail.com>
2022-07-11 12:28:31 -0500

    Add first & returning app by channel queries

---
+++ first_app_channel.sql
@@ -0,0 +1,39 @@
+/* AA : AA Main dashboard : first general applications : prod */
+SELECT
+    str_to_date(concat(yearweek(oc.interested), ' Sunday'),'%X%V %W') AS date,
+    tc.utm_medium AS UTM,
+    count(distinct oc.id) AS new_applications
+FROM
+    opportunity_candidates oc 
+    INNER JOIN opportunities o ON oc.opportunity_id = o.id 
+    LEFT JOIN tracking_code_candidates tcc ON oc.id = tcc.candidate_id
+    LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id 
+WHERE
+    oc.interested IS NOT NULL 
+    AND oc.interested > '2021-7-18'
+    AND o.objective NOT LIKE '**%'
+    AND o.id IN (
+        SELECT
+            DISTINCT o.id AS opportunity_id
+        FROM
+            opportunities o
+            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
+            AND omp.poster = TRUE
+            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
+            AND pf.opportunity_crawler = FALSE
+        WHERE
+            o.reviewed >= '2021/01/01'
+            AND o.objective NOT LIKE '**%'
+            AND o.review = 'approved'
+    )
+    AND oc.id IN (
+        SELECT
+            min(oc.id)
+        FROM
+            opportunity_candidates oc
+        GROUP BY
+            oc.person_id
+    )
+GROUP BY 
+    str_to_date(concat(yearweek(oc.interested), ' Sunday'),'%X%V %W'),
+    tc.utm_medium
