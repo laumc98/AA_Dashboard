1bfea49e036b4d4eb8603221cd48b49cd2a8dd18
laumc98 <lanaca98@gmail.com>
2022-08-11 12:18:13 -0500

    Update awareness.sql

--- awareness.sql
+++ awareness.sql
@@ -1,7 +1,8 @@
 /* AA : AA Main dashboard : awareness : prod */ 
 SELECT
     `metrics_users`.`id` AS `SubjectID`,
-    `metrics_users`.`awareness_answer` AS `answer`
+    `metrics_users`.`awareness_answer` AS `answer`,
+    `metrics_users`.`locale` as `locale`
 FROM
     `metrics_users`
     LEFT JOIN `users_user` `Users User` ON `metrics_users`.`id` = `Users User`.`id`
