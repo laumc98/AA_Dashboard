b4f4cbbca23525512a8bd9e85612ac53353c40c9
laumc98 <lanaca98@gmail.com>
2022-08-11 14:17:54 -0500

    Update list_applicants.sql

--- list_applicants.sql
+++ list_applicants.sql
@@ -1,11 +1,12 @@
 /* AA : AA Main dashboard : list app : prod */ 
 SELECT
    `People`.`subject_identifier` AS `SubjectID`,
-   `opportunity_candidates`.`id` AS `id`,
    `opportunity_candidates`.`interested` AS `interested`,
+   `o`.`fulfillment`,
    `People`.`email` AS `email`
 FROM
    `opportunity_candidates`
+   LEFT JOIN `opportunities` `o` on `opportunity_candidates`.`opportunity_id` = `o`.`id`
    LEFT JOIN `opportunity_members` `Opportunity Members - Opportunity` ON `opportunity_candidates`.`opportunity_id` = `Opportunity Members - Opportunity`.`opportunity_id`
    LEFT JOIN `person_flags` `Person Flags - Person` ON `Opportunity Members - Opportunity`.`person_id` = `Person Flags - Person`.`person_id`
    LEFT JOIN `people` `People` ON `opportunity_candidates`.`person_id` = `People`.`id`
