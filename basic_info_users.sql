abcd249b9dbb8fdb09a14bf14a2b5139d8a7328b
laumc98 <lanaca98@gmail.com>
2022-09-12 17:40:58 -0500

    Revert "Update basic_info_users.sql"
    This reverts commit 790b7074506b5bce50104dcd00d06ddb125f40e8.

--- basic_info_users.sql
+++ basic_info_users.sql
@@ -4,22 +4,11 @@ SELECT
     people.id AS `Person ID`,
     people.current_role AS `role`,
     person_location.country AS `Country`,
-    person_stats.completion_global AS `genome_completion`,
-    languages.person_id AS `Person ID`,
-    languages.language AS `Languages`,
-    languages.fluency AS `Languages__fluency`,
-    person_segments.name AS `segments`,
-    strengths.code AS `Skill ID`,
-    strengths.proficiency AS `Skill_proficiency`
+    person_stats.completion_global AS `genome_completion`
 FROM
     people
     LEFT JOIN person_location ON people.id = person_location.person_id
     LEFT JOIN person_stats ON people.id = person_stats.person_id
-    LEFT JOIN languages ON people.id = languages.person_id 
-    LEFT JOIN person_segments ON people.id = person_segments.person_id 
-    LEFT JOIN strengths ON people.id = strengths.person_id 
 WHERE 
-    DATE(people.created) >= date(date_add(now(6), INTERVAL -400 day))
-    AND strengths.active = TRUE
-    AND languages.active = TRUE
+    DATE(people.created) >= date(date_add(now(6), INTERVAL -360 day))
 ORDER BY people.id DESC
