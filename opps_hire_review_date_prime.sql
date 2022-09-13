bea9c8d9662ee55e3bdcc224b17dbcd9e6d009d4
laumc98 <lanaca98@gmail.com>
2022-09-13 10:14:47 -0500

    Fullfilment values

--- opps_hire_review_date_prime.sql
+++ opps_hire_review_date_prime.sql
@@ -9,7 +9,7 @@ WHERE
    (
       `opportunities__via__opportunit`.`reviewed` > "2021-7-18"
       AND `opportunities__via__opportunit`.`reviewed` < date(now(6))
-      AND (`opportunities`.`fulfillment` like '%prime%' OR `opportunities`.`fulfillment` like '%agile%')
+      AND (`opportunities__via__opportunit`.`fulfillment` like '%prime%' OR `opportunities__via__opportunit`.`fulfillment` like '%agile%')
    )
 GROUP BY
    str_to_date(concat(yearweek(`opportunity_operational_hires`.`hiring_date`),' Sunday'),'%X%V %W')
