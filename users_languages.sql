cccab51640849655ac3904222064bddd2f171eda
laumc98 <lanaca98@gmail.com>
2022-09-15 15:08:24 -0500

    date again

--- users_languages.sql
+++ users_languages.sql
@@ -10,6 +10,6 @@ FROM
 WHERE
     (
         languages.active = TRUE
-        AND DATE(people.created) >= date(date_add(now(6), INTERVAL -360 day))
+        AND DATE(people.created) >= date(date_add(now(6), INTERVAL -300 day))
     )
 ORDER BY languages.person_id DESC
