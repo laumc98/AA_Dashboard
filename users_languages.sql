b71f18465fbcef762f3244b86ea1cab3cc955d6c
laumc98 <lanaca98@gmail.com>
2022-09-16 09:58:37 -0500

    date segmentation

--- users_languages.sql
+++ users_languages.sql
@@ -10,6 +10,6 @@ FROM
 WHERE
     (
         languages.active = TRUE
-        AND DATE(people.created) >= date(date_add(now(6), INTERVAL -300 day))
+        AND DATE(people.created) >= date(date_add(now(6), INTERVAL -200 day))
     )
 ORDER BY languages.person_id DESC
