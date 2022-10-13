9efd1a945d660be13af58f794bc87f75249fa575
laumc98 <lanaca98@gmail.com>
2022-10-13 10:17:03 -0500

    more reviewed

--- opps_3mm_prime.sql
+++ opps_3mm_prime.sql
@@ -34,7 +34,7 @@ FROM
                                 SELECT
                                     oc.opportunity_id,
                                     occh.created,
-                                    o.reviewed,
+                                    o.last_reviewed,
                                     oc.name
                                 FROM
                                     opportunity_candidate_column_history occh
@@ -64,13 +64,13 @@ FROM
                                             INNER JOIN person_flags pf ON pf.person_id = omp.person_id
                                             AND pf.opportunity_crawler = FALSE
                                         WHERE
-                                            o.reviewed >= '2021/01/01'
+                                            o.last_reviewed >= '2021/01/01'
                                             AND o.objective NOT LIKE '**%'
                                             AND o.review = 'approved'
                                     )
                                 ORDER BY
                                     opportunity_id,
-                                    o.reviewed
+                                    o.last_reviewed
                             ) AS matches
                             CROSS JOIN (
                                 SELECT
