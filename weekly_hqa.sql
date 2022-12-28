5e2d094cdbd3694ea6a6fe35a8667fd872859c5c
laumc98 <lanaca98@gmail.com>
2022-12-28 12:08:16 -0500

    Update weekly_hqa.sql

--- weekly_hqa.sql
+++ weekly_hqa.sql
@@ -3,7 +3,7 @@ SELECT
     str_to_date(concat(yearweek(applications.timestamp), ' Sunday'),'%X%V %W') AS 'date',
     applications.opportunity_reference_id AS 'Alfa ID',
     applications.utm_medium AS 'utm_medium',
-    count(distinct applications.gg_id) AS 'HQA'
+    count(distinct applications.id) AS 'HQA'
 FROM
     applications
     INNER JOIN opportunity ON applications.opportunity_reference_id = opportunity.ref_id
