35d183cd75156045decd39a9eafe92a2e0117fa9
laumc98 <lanaca98@gmail.com>
2023-03-20 10:54:32 -0500

    Update mm queries to take the min date

--- weekly_mm_channel_appdate.sql
+++ weekly_mm_channel_appdate.sql
@@ -1,39 +1,52 @@
 /* AA : AA Main dashboard : weekly mm per channel by app date : prod */ 
 SELECT
-    str_to_date(concat(yearweek(oca.interested), ' Sunday'),'%X%V %W') AS 'date',
-    oca.opportunity_id AS 'ID',
-    tc.utm_medium AS 'Tracking Codes__utm_medium',
-    count(distinct oca.id) AS 'weekly_mm_channel_appdate'
+    str_to_date(concat(yearweek(mutual_matches.interested), ' Sunday'),'%X%V %W') AS 'date',
+    mutual_matches.opportunity_id AS 'ID',
+    mutual_matches.utm_medium AS 'Tracking Codes__utm_medium',
+    count(*) AS 'weekly_mm_channel_appdate'
 FROM
-    opportunity_candidates oca 
-    INNER JOIN opportunities o ON oca.opportunity_id = o.id 
-    LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
-    LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
-    LEFT JOIN opportunity_candidate_column_history occh ON oca.id = occh.candidate_id
-    LEFT JOIN opportunity_columns oc ON occh.to = oc.id
+(
+    SELECT 
+        occh.candidate_id,
+        oca.opportunity_id,
+        tc.utm_medium,
+        oca.interested,
+        MIN(occh.created) AS created
+    FROM
+        opportunity_candidate_column_history occh
+        INNER JOIN opportunity_columns oc ON occh.to = oc.id
+        INNER JOIN opportunities o ON oc.opportunity_id = o.id
+        LEFT JOIN opportunity_candidates oca ON occh.candidate_id = oca.id
+        LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
+        LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
+    WHERE
+        oc.name = 'mutual matches'
+        AND occh.created >= '2021-07-18'
+        AND oca.interested IS NOT NULL 
+        AND o.objective NOT LIKE '**%'
+        AND oca.application_step IS NOT NULL
+        AND o.id IN (
+            SELECT
+                DISTINCT o.id AS opportunity_id
+            FROM
+                opportunities o
+                INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
+                AND omp.poster = TRUE
+                INNER JOIN person_flags pf ON pf.person_id = omp.person_id
+                AND pf.opportunity_crawler = FALSE
+            WHERE
+                date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= '2021-01-1'
+                AND o.objective NOT LIKE '**%'
+                AND o.review = 'approved'
+        )
+    GROUP BY
+        occh.candidate_id,
+        oca.opportunity_id,
+        tc.utm_medium
+) AS mutual_matches
 WHERE
-    oca.interested IS NOT NULL 
-    AND oc.name = 'mutual matches'
-    AND occh.created IS NOT NULL
-    AND oca.interested > '2021-7-18'
-    AND o.objective NOT LIKE '**%'
-    AND oca.application_step IS NOT NULL
-    AND date(occh.created) <= date(oca.interested) + 7
-    AND o.id IN (
-        SELECT
-            DISTINCT o.id AS opportunity_id
-        FROM
-            opportunities o
-            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
-            AND omp.poster = TRUE
-            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
-            AND pf.opportunity_crawler = FALSE
-        WHERE
-            date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= '2021/01/01'
-            AND o.objective NOT LIKE '**%'
-            AND o.review = 'approved'
-    )
+    date(mutual_matches.created) <= date(mutual_matches.interested) + 7
 GROUP BY 
-    str_to_date(concat(yearweek(oca.interested), ' Sunday'),'%X%V %W'),
-    tc.utm_medium,
-    oca.opportunity_id
+    str_to_date(concat(yearweek(mutual_matches.interested), ' Sunday'),'%X%V %W'),
+    mutual_matches.opportunity_id,
+    mutual_matches.utm_medium
--- weekly_ready_interview_mmdate.sql
+++ weekly_ready_interview_mmdate.sql
@@ -1,50 +1,67 @@
 /* AA : AA Main dashboard : weekly ready for interview by app date: prod */
 SELECT
-    str_to_date(concat(yearweek(mutual_matches.mm_date), ' Sunday'),'%X%V %W') AS 'date',
-    o.id AS 'ID',
-    o.fulfillment AS 'fulfillment',
-    tc.utm_medium AS 'Tracking Codes__utm_medium',
-    count(distinct occh.candidate_id) AS 'weekly_ready_interview_mmdate'
+    str_to_date(concat(yearweek(ready_for_interview.created), ' Sunday'),'%X%V %W') AS 'date',
+    ready_for_interview.opportunity_id AS 'ID',
+    ready_for_interview.fulfillment AS 'fulfillment',
+    ready_for_interview.utm_medium AS 'Tracking Codes__utm_medium',
+    count(*) AS 'weekly_ready_interview'
 FROM
-    (
-        SELECT
-            date(occh.created) AS 'mm_date',
-            occh.candidate_id
-        FROM
-            opportunity_candidate_column_history occh
-            INNER JOIN opportunity_columns oc ON occh.to = oc.id
-        WHERE
-            oc.name = 'mutual matches'
-            AND date(occh.created) >= '2021-07-18'
-    ) AS mutual_matches
-    INNER JOIN opportunity_candidate_column_history occh ON mutual_matches.candidate_id = occh.candidate_id
-    INNER JOIN opportunity_columns oc ON occh.to = oc.id
-    INNER JOIN opportunities o ON oc.opportunity_id = o.id
-    LEFT JOIN opportunity_candidates oca ON occh.candidate_id = oca.id
-    LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
-    LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
-WHERE
-    oc.funnel_tag = 'ready_for_interview'
-    AND o.objective NOT LIKE '**%'
-    AND date(occh.created) <= date(mutual_matches.mm_date) + 7
-    AND o.id IN (
-        SELECT
-            DISTINCT o.id AS opportunity_id
-        FROM
-            opportunities o
-            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
-            AND omp.poster = TRUE
-            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
-            AND pf.opportunity_crawler = FALSE
-        WHERE
-            date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= '2021/01/01'
-            AND o.objective NOT LIKE '**%'
-            AND o.review = 'approved'
-    )
-GROUP BY
-    str_to_date(concat(yearweek(mutual_matches.mm_date), ' Sunday'),'%X%V %W'),
-    tc.utm_medium,
-    o.id,
-    o.fulfillment
-ORDER BY
-    str_to_date(concat(yearweek(mutual_matches.mm_date), ' Sunday'),'%X%V %W') ASC
+(
+    SELECT 
+        occh.candidate_id,
+        oca.opportunity_id,
+        o.fulfillment,
+        tc.utm_medium,
+        mutual_matches.mm_date,
+        MIN(occh.created) AS created
+    FROM
+        (
+            SELECT
+                occh.candidate_id,
+                min(occh.created) AS 'mm_date'
+            FROM
+                opportunity_candidate_column_history occh
+                INNER JOIN opportunity_columns oc ON occh.to = oc.id
+            WHERE
+                oc.name = 'mutual matches'
+                AND date(occh.created) >= date(date_add(now(6), INTERVAL -1 year))
+            GROUP BY 
+                occh.candidate_id
+        ) AS mutual_matches
+        INNER JOIN opportunity_candidate_column_history occh
+        INNER JOIN opportunity_columns oc ON occh.to = oc.id
+        INNER JOIN opportunities o ON oc.opportunity_id = o.id
+        LEFT JOIN opportunity_candidates oca ON occh.candidate_id = oca.id
+        LEFT JOIN tracking_code_candidates tcc ON oca.id = tcc.candidate_id
+        LEFT JOIN tracking_codes tc ON tcc.tracking_code_id = tc.id
+    WHERE
+        oc.funnel_tag = 'ready_for_interview'
+        AND occh.created >= date(date_add(now(6), INTERVAL -1 year))
+        AND oca.interested IS NOT NULL 
+        AND o.objective NOT LIKE '**%'
+        AND o.id IN (
+            SELECT
+                DISTINCT o.id AS opportunity_id
+            FROM
+                opportunities o
+                INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
+                AND omp.poster = TRUE
+                INNER JOIN person_flags pf ON pf.person_id = omp.person_id
+                AND pf.opportunity_crawler = FALSE
+            WHERE
+                date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= date(date_add(now(6), INTERVAL -1 year))
+                AND o.objective NOT LIKE '**%'
+                AND o.review = 'approved'
+        )
+    GROUP BY
+        occh.candidate_id,
+        oca.opportunity_id,
+        o.fulfillment,
+        tc.utm_medium
+) AS ready_for_interview
+WHERE 
+    date(ready_for_interview.created) <= date(ready_for_interview.mm_date) + 7
+GROUP BY 
+    str_to_date(concat(yearweek(ready_for_interview.created), ' Sunday'),'%X%V %W'),
+    ready_for_interview.opportunity_id,
+    ready_for_interview.utm_medium
