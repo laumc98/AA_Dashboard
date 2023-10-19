1ba4a957e0bf8310bc743cdb1909ba5fe41ef740
laumc98 <lanaca98@gmail.com>
2023-10-19 14:07:33 -0500

    Update weekly_hqa.sql

--- weekly_hqa.sql
+++ weekly_hqa.sql
@@ -7,11 +7,11 @@ SELECT
 FROM
     applications
     INNER JOIN opportunity ON applications.opportunity_reference_id = opportunity.ref_id
+    LEFT JOIN mutual_matches ON (mutual_matches.gg_id = applications.gg_id AND mutual_matches.opportunity_reference_id = applications.opportunity_reference_id)
 WHERE
-    (
-        applications.filters_passed = TRUE
-        AND JSON_EXTRACT(opportunity.opportunity_snapshot, '$."crawled"') = FALSE
-    )
+    (applications.filters_passed = TRUE
+        OR mutual_matches.timestamp IS NOT NULL)
+    AND JSON_EXTRACT(opportunity.opportunity_snapshot, '$."crawled"') = FALSE
 GROUP BY 
     str_to_date(concat(yearweek(applications.timestamp), ' Sunday'),'%X%V %W'),
     applications.opportunity_reference_id,
