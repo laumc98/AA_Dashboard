6d600dd63e1166dedb4ff5dc2278d5b00e11af95
laumc98 <lanaca98@gmail.com>
2023-10-23 12:01:45 -0500

    Fix queries

--- opps_3mm7days.sql
+++ opps_3mm7days.sql
@@ -1,180 +1,85 @@
 /* AA : AA Main dashboard : opps with 3+mm 7 days : prod */ 
 SELECT
-    str_to_date(concat(yearweek(dt.reviewed), ' Sunday'),'%X%V %W') AS date,
-    dt.fulfillment,
-    wk.opportunities AS opps_3mm_weekly_7days
+    str_to_date(concat(yearweek(reviewed_date), ' Sunday'),'%X%V %W') AS date,
+    fulfillment,
+    COUNT(*) AS opps_3mm_weekly_7days
 FROM
     (
         SELECT
-            YEAR(reviewed_date) AS year,
-            WEEK(reviewed_date) AS week,
-            DATE(reviewed_date) AS reviewed,
+            opportunity_id,
             fulfillment,
-            COUNT(*) AS opportunities
+            created AS match_date,
+            last_reviewed AS reviewed_date
         FROM
             (
                 SELECT
-                    opportunity_id,
-                    fulfillment,
-                    created AS match_date,
-                    last_reviewed AS reviewed_date
+                    matches.*,
+                    IF(
+                        @index <> matches.opportunity_id,
+                        @sum := 1,
+                        @sum := @sum + 1
+                    ) AS match_sum,
+                    IF(
+                        @index <> matches.opportunity_id,
+                        @index := matches.opportunity_id,
+                        @index := @index
+                    ) AS idd
                 FROM
                     (
                         SELECT
-                            matches.*,
-                            IF(
-                                @index <> matches.opportunity_id,
-                                @sum := 1,
-                                @sum := @sum + 1
-                            ) AS match_sum,
-                            IF(
-                                @index <> matches.opportunity_id,
-                                @index := matches.opportunity_id,
-                                @index := @index
-                            ) AS idd
+                            oc.opportunity_id,
+                            occh.candidate_id,
+                            min(occh.created) AS created,
+                            date(coalesce(null, o.first_reviewed, o.last_reviewed)) as last_reviewed,
+                            o.fulfillment
                         FROM
-                            (
+                            opportunity_candidate_column_history occh
+                            INNER JOIN opportunity_columns oc ON occh.to = oc.id
+                            INNER JOIN opportunities o ON oc.opportunity_id = o.id
+                        WHERE
+                            oc.name = 'mutual matches'
+                            AND occh.created >= '2021-01-01'
+                            AND datediff(date(occh.created),date(coalesce(null, o.first_reviewed, o.last_reviewed))) <= 7
+                            AND o.objective NOT LIKE '**%'
+                            AND o.crawled = FALSE
+                            AND o.id NOT IN (
                                 SELECT
-                                    oc.opportunity_id,
-                                    occh.created,
-                                    o.fulfillment,
-                                    date(coalesce(null, o.first_reviewed, o.last_reviewed)) as last_reviewed,
-                                    oc.name
+                                    DISTINCT opportunity_id
                                 FROM
-                                    opportunity_candidate_column_history occh
-                                    INNER JOIN opportunity_columns oc ON occh.to = oc.id
-                                    INNER JOIN opportunities o ON oc.opportunity_id = o.id
+                                    opportunity_organizations oorg
                                 WHERE
-                                    oc.name = 'mutual matches'
-                                    AND occh.created >= '2021-01-01'
-                                    AND datediff(date(occh.created), date(coalesce(null, o.first_reviewed, o.last_reviewed))) <= 7
-                                    AND o.objective NOT LIKE '**%'
-                                    AND o.crawled = FALSE
-                                    AND o.id NOT IN (
-                                        SELECT
-                                            DISTINCT opportunity_id
-                                        FROM
-                                            opportunity_organizations oorg
-                                        WHERE
-                                            oorg.organization_id = '748404'
-                                            AND oorg.active
-                                    )
-                                    AND o.id IN (
-                                        SELECT
-                                            DISTINCT o.id AS opportunity_id
-                                        FROM
-                                            opportunities o
-                                            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
-                                            AND omp.poster = TRUE
-                                            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
-                                            AND pf.opportunity_crawler = FALSE
-                                        WHERE
-                                            date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= '2021/01/01'
-                                            AND o.objective NOT LIKE '**%'
-                                            AND o.review = 'approved'
-                                    )
-                                ORDER BY
-                                    opportunity_id,
-                                    date(coalesce(null, o.first_reviewed, o.last_reviewed))
-                            ) AS matches
-                            CROSS JOIN (
+                                    oorg.organization_id = '748404'
+                                    AND oorg.active
+                                )
+                            AND o.id IN (
                                 SELECT
-                                    @sum := 0,
-                                    @index := 0
-                            ) counter
-                    ) numbered
-                WHERE
-                    match_sum = 3
-            ) groupped
-        GROUP BY
-            year,
-            week,
-            reviewed,
-            fulfillment
-    ) AS dt
-    INNER JOIN (
-        SELECT
-            YEAR(reviewed_date) AS year,
-            WEEK(reviewed_date) AS week,
-            COUNT(*) AS opportunities
-        FROM
-            (
-                SELECT
-                    opportunity_id,
-                    created AS match_date,
-                    last_reviewed AS reviewed_date
-                FROM
-                    (
-                        SELECT
-                            matches.*,
-                            IF(
-                                @index <> matches.opportunity_id,
-                                @sum := 1,
-                                @sum := @sum + 1
-                            ) AS match_sum,
-                            IF(
-                                @index <> matches.opportunity_id,
-                                @index := matches.opportunity_id,
-                                @index := @index
-                            ) AS idd
-                        FROM
-                            (
-                                SELECT
-                                    oc.opportunity_id,
-                                    occh.created,
-                                    date(coalesce(null, o.first_reviewed, o.last_reviewed)) as last_reviewed,
-                                    oc.name
+                                    DISTINCT o.id AS opportunity_id
                                 FROM
-                                    opportunity_candidate_column_history occh
-                                    INNER JOIN opportunity_columns oc ON occh.to = oc.id
-                                    INNER JOIN opportunities o ON oc.opportunity_id = o.id
+                                    opportunities o
+                                    INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
+                                    AND omp.poster = TRUE
+                                    INNER JOIN person_flags pf ON pf.person_id = omp.person_id
+                                    AND pf.opportunity_crawler = false
                                 WHERE
-                                    oc.name = 'mutual matches'
-                                    AND occh.created >= '2021-01-01'
-                                    AND datediff(date(occh.created), date(coalesce(null, o.first_reviewed, o.last_reviewed))) <= 7
+                                    o.last_reviewed >= '2021/01/01'
                                     AND o.objective NOT LIKE '**%'
-                                    AND o.crawled = FALSE
-                                    AND o.id NOT IN (
-                                        SELECT
-                                            DISTINCT opportunity_id
-                                        FROM
-                                            opportunity_organizations oorg
-                                        WHERE
-                                            oorg.organization_id = '748404'
-                                            AND oorg.active
-                                    )
-                                    AND o.id IN (
-                                        SELECT
-                                            DISTINCT o.id AS opportunity_id
-                                        FROM
-                                            opportunities o
-                                            INNER JOIN opportunity_members omp ON omp.opportunity_id = o.id
-                                            AND omp.poster = TRUE
-                                            INNER JOIN person_flags pf ON pf.person_id = omp.person_id
-                                            AND pf.opportunity_crawler = FALSE
-                                        WHERE
-                                            date(coalesce(null, o.first_reviewed, o.last_reviewed)) >= '2021/01/01'
-                                            AND o.objective NOT LIKE '**%'
-                                            AND o.review = 'approved'
-                                    )
-                                ORDER BY
-                                    opportunity_id,
-                                    date(coalesce(null, o.first_reviewed, o.last_reviewed))
-                            ) AS matches
-                            CROSS JOIN (
-                                SELECT
-                                    @sum := 0,
-                                    @index := 0
-                            ) counter
-                    ) numbered
-                WHERE
-                    match_sum = 3
-            ) groupped
-        GROUP BY
-            year,
-            week
-    ) AS wk ON wk.year = dt.year
-    AND wk.week = dt.week
+                                    AND o.review = 'approved'
+                                )
+                            GROUP BY
+                                occh.candidate_id
+                            ORDER BY
+                                opportunity_id,
+                                created
+                    ) AS matches
+                    CROSS JOIN (
+                            SELECT
+                                @sum := 0,
+                                @index := 0
+                        ) counter
+            ) numbered
+        WHERE
+            match_sum = 3
+    ) groupped
 GROUP BY
-    str_to_date(concat(yearweek(dt.reviewed), ' Sunday'),'%X%V %W'),
-    dt.fulfillment
+    date,
+    fulfillment
